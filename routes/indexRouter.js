e6da05f923c1557620e8a8cca6cc30b65124af49
jimyeong <idjjm92@gmail.com>
2021-08-09 11:16:36 +0900

    db hasone relation handleing

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,13 +1,35 @@
 const router = require("express").Router();
-
+const User = require("../models/user");
+const Mentor = require("../models/mentor");
 
 router.post("/register", (req, res, next)=>{
+    const {email, password} = req.body;
     console.log("hello world");
+    const user = User.create({
+        email, password
+    });
+    req.user = user;
     next();
 }, (req, res)=>{
-    res.status(200).json({
-        message: "hi"
-    })
+    const setMentor = (user)=>{
+        return Mentor.create({
+            userId: user.id 
+        });
+    }
+    const onSuccess = ()=>{
+        res.status(200).json({
+            message: "success"
+        });
+    };
+    const onFail = (err)=>{
+        res.status(400).json({
+            message: err.message
+        })
+    }
+    req.user
+    .then(setMentor)
+    .then(onSuccess)
+    .catch(onFail);
 })
 
 
